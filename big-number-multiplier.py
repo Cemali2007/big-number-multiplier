56ca38508061c293f908643ad390066ecba68dbb
czeyt <githubda-kullandigin@email.com>
2026-01-20 23:25:55 +0300

    Initial commit

---
+++ big-number-multiplier.py
@@ -0,0 +1,16 @@
+
+
+
+num1 = int (input("Please Enter The First Number You Want To Multiply And Press Enter :"))
+print ("")
+num2 = int (input("Please Enter The Second Number You Want To Multiply And Press Enter :"))
+
+num1 *= num2
+
+print ("")
+print ("")
+print ("")
+print(f"Result ====> {num1}")
+print ("")
+print ("")
+input ("Thank You For Choosing Us. Please Press Enter To Exit The Calculator... ")
